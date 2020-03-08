f620ff63dc2cafecb078fdeae4ebfab10125284b
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-03-08 18:07:45 +0000

    Fix contents about numbers

---
+++ 02-core/02-numbers/solutions/bases.py
@@ -0,0 +1,11 @@
+value = '0x3f81'
+
+value_as_int = int(value, 16)
+value_as_bin = bin(value_as_int)
+value_as_oct = oct(value_as_int)
+value_as_hex = hex(value_as_int)
+
+print(value_as_int)
+print(value_as_bin)
+print(value_as_oct)
+print(value_as_hex)
