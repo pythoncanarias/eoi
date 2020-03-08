e50194b6346b157af5339d3c3c7ecb9ff3c1d9ed
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-03-08 19:49:03 +0000

    Fix contents about strings

---
+++ 02-core/04-strings/solutions/fstrings.py
@@ -0,0 +1,7 @@
+E = 2.71828
+
+print(f'{E:.3f}')
+print(f'{E:8.2f}')
+print(f'{E:e}')
+print(f'{E:010.4f}')
+print(f'{E:20.5f}')
---
+++ 02-core/04-strings/solutions/transform.py
@@ -0,0 +1,7 @@
+quote = '  \n\t\nCommon sense is genius dressed in its working clothes\n\n  \t'
+
+quote = quote.strip()
+quote = quote.replace('dressed in', 'with')
+quote = quote.title()
+
+print(quote)
