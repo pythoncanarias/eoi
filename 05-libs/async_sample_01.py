fa471ab692bac4d2221d8e149f8513a9ba5f93eb
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2020-04-12 00:28:03 +0100

    Multitask programming examples

---
+++ 05-libs/async_sample_01.py
@@ -0,0 +1,25 @@
+import requests
+import time
+
+
+def download_site(url, session):
+    with session.get(url) as response:
+        print(f"Read {len(response.content)} from {url}")
+
+
+def download_all_sites(sites):
+    with requests.Session() as session:
+        for url in sites:
+            download_site(url, session)
+
+
+if __name__ == "__main__":
+    sites = [
+        "http://www.pythoncanarias.es",
+        "http://www.python.org",
+        "http://olympus.realpython.org/dice",
+    ]
+    start_time = time.time()
+    download_all_sites(sites)
+    duration = time.time() - start_time
+    print(f"Downloaded {len(sites)} in {duration} seconds")
