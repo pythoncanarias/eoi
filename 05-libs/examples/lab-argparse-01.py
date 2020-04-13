8a50295d79ba3ac9d1bdd1db6c396abf995e79de
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2020-04-13 15:07:25 +0100

    Add module argparse from standard library

---
+++ 05-libs/examples/lab-argparse-01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.parse_args()
+
---
+++ 05-libs/examples/lab-argparse-04.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("target", help="texto a mostrar en la pantalla")
+options = parser.parse_args()
+print(options.target.upper())
+
