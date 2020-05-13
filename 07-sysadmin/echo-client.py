e13ae0457f66068848f3b9f36f7bd43bcc3b7507
Alejandro Samarín <alejandro.samarin@gmail.com>
2020-05-13 20:15:46 +0100

    echo-client input loop

--- 07-sysadmin/echo-client.py
+++ 07-sysadmin/echo-client.py
@@ -6,7 +6,11 @@ PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+    while True:
+        string = input('> ')
+        s.sendall(string.encode('utf-8'))
+        data = s.recv(1024)
+        if data == b'exit':
+            break
 
 print('Received', repr(data))
