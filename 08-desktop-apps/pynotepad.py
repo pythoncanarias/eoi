f66a440d7ed3ae51f43f2479b8d037ba916cf393
Alejandro Samarín <alejandro.samarin@gmail.com>
2020-05-22 18:45:45 +0100

    add "about" dialog

--- 08-desktop-apps/pynotepad.py
+++ 08-desktop-apps/pynotepad.py
@@ -12,5 +12,20 @@ close_action = QAction("&Close")
 close_action.triggered.connect(window.close)
 file_menu.addAction(close_action)
 
+def show_about_dialog():
+    text = """
+        <center>
+            <h1>PyNotepad</h1><br/>
+            <img src=logo.png width=200 height=200>
+        </center>
+        <p>Version 0.0.1</p>
+    """
+    QMessageBox.about(window, "About PyNotepad", text)
+
+help_menu = window.menuBar().addMenu("&Help")
+about_action = QAction("&About")
+about_action.triggered.connect(show_about_dialog)
+help_menu.addAction(about_action)
+
 window.show()
 app.exec()
